590f9cd9aee37bb5b0e0f940a55365fbd11f3e3c
jun68ykt <jun68ykt@gmail.com>
2019-04-27 14:33:35 +0900

    SideBarをFunctional Componentに修正

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -5,39 +5,33 @@ const FILTER_VALUES = [
   { name: 'size', values: ['S', 'M', 'L'] }
 ];
 
-class SideBar extends React.Component {
-  render() {
-    const { filter, onChange, onClear } = this.props;
-
-    return (
-      <div>
-        {
-          FILTER_VALUES.map(({name, values}) =>
-            <ul
-              className={`${name}_list`}
-              key={name}
-            >
-              {
-                values.map(v =>
-                  <li key={v}>
-                    <input
-                      name={name}
-                      type="radio"
-                      value={v}
-                      checked={filter[name] === v}
-                      onChange={onChange}
-                    />
-                    <span className='capitalize'>{v}</span>
-                  </li>
-                )
-              }
-            </ul>
-          )
-        }
-        <button onClick={onClear}>フィルタをクリア</button>
-      </div>
-    );
-  }
-}
+const SideBar = ({ filter, onChange, onClear }) => (
+  <div>
+    {
+      FILTER_VALUES.map(({name, values}) =>
+        <ul
+          className={`${name}_list`}
+          key={name}
+        >
+          {
+            values.map(v =>
+              <li key={v}>
+                <input
+                  name={name}
+                  type="radio"
+                  value={v}
+                  checked={filter[name] === v}
+                  onChange={onChange}
+                />
+                <span className='capitalize'>{v}</span>
+              </li>
+            )
+          }
+        </ul>
+      )
+    }
+    <button onClick={onClear}>フィルタをクリア</button>
+  </div>
+);
 
 export default SideBar;
