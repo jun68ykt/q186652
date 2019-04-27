74e7be7b7c8847e61fd67e734176853a8924a92a
jun68ykt <jun68ykt@gmail.com>
2019-04-27 13:34:58 +0900

    SideBarリファクタ

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,80 +1,39 @@
 import React from 'react'
 
+const filterValues = [
+  { name: 'category', values: ['shirt', 'jacket', 'skirt', 'pants'] },
+  { name: 'size', values: ['S', 'M', 'L'] }
+];
+
 class SideBar extends React.Component {
   render() {
     const { filter, onChange, onClear } = this.props;
 
     return (
       <div>
-
-        <ul className="category_list">
-          <li>
-            <input
-              name="category"
-              type="radio"
-              value="shirt"
-              checked={filter.category === 'shirt'}
-              onChange={onChange}
-            />Shirts
-          </li>
-          <li>
-            <input
-              name="category"
-              type="radio"
-              value="jacket"
-              checked={filter.category === 'jacket'}
-              onChange={onChange}
-            />Jackets
-          </li>
-          <li>
-            <input
-              name="category"
-              type="radio"
-              value="skirt"
-              checked={filter.category === 'skirt'}
-              onChange={onChange}
-            />Skirts
-          </li>
-          <li>
-            <input
-              name="category"
-              type="radio"
-              value="pants"
-              checked={filter.category === 'pants'}
-              onChange={onChange}
-            />Pants
-          </li>
-        </ul>
-
-        <ul className="size_list">
-          <li>
-            <input
-              name="size"
-              type="radio"
-              value="S"
-              checked={filter.size === 'S'}
-              onChange={onChange}
-            />S
-          </li>
-          <li>
-            <input
-              name="size"
-              type="radio"
-              value="M"
-              checked={filter.size === 'M'}
-              onChange={onChange}
-            />M
-          </li>
-          <li>
-            <input
-              name="size"
-              type="radio"
-              value="L"
-              checked={filter.size === 'L'}
-              onChange={onChange}
-            />L
-          </li>
-        </ul>
+        {
+          filterValues.map(({name, values}) =>
+            <ul
+              className={`${name}_list`}
+              key={name}
+            >
+              {
+                values.map(v =>
+                  <li key={v}>
+                    <input
+                      name={name}
+                      type="radio"
+                      value={v}
+                      checked={filter[name] === v}
+                      onChange={onChange}
+                    />
+                    <span className='capitalize'>{v}</span>
+                  </li>
+                )
+              }
+            </ul>
+          )
+        }
         <button onClick={onClear}>フィルタをクリア</button>
       </div>
     );
